172241cb8a170705700cc082a60b3ac7c9fd3bad
paulo <paulo.chiaradia@outlook.com>
2024-06-12 19:23:00 -0300

    Maps

--- Secao3/maps/maps.go
+++ Secao3/maps/maps.go
@@ -6,13 +6,17 @@ func main() {
 	m := make(map[string]int)
 	m["sp"] = 100
 	m["rj"] = 90
+	m["cg"] = 50
 	fmt.Println(m["rj"])
 
-	valor, existe := m["mg"]
-	if existe {
+	if valor, existe := m["mg"]; existe {
 		fmt.Println(valor)
 	} else {
 		fmt.Println("Nao existe")
 		fmt.Println(existe)
+	}
+	delete(m, "cg")
+	for chave, valor := range m {
+		fmt.Println("Cidade:", chave, "Habitantes: ", valor)
 	}
 }
