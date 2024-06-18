4517d1215b33b2e7b52f6459823bb0469c31bdb8
paulo <paulo.chiaradia@outlook.com>
2024-06-18 19:44:32 -0300

    Pacote Externo

---
+++ Secao12/PacotesExternos/pacoteExterno.go
@@ -0,0 +1,22 @@
+package main
+
+import "github.com/fatih/color"
+
+func main() {
+	// Create a new color object
+	c := color.New(color.FgCyan).Add(color.Underline)
+	c.Println("Prints cyan text with an underline.")
+
+	// Or just add them to New()
+	d := color.New(color.FgCyan, color.Bold)
+	d.Printf("This prints bold cyan %s\n", "too!.")
+
+	// Mix up foreground and background colors, create new mixes!
+	red := color.New(color.FgRed)
+
+	boldRed := red.Add(color.Bold)
+	boldRed.Println("This will print text in bold red.")
+
+	whiteBackground := red.Add(color.BgWhite)
+	whiteBackground.Println("Red text with white background.")
+}
